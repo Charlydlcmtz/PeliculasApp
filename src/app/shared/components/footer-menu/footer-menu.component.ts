a7e5a99c81e2b0715fb9c28ff5728c5d01264ee4
Charlydlcmtz <carlos.cdlcmtz@gmail.com>
2025-03-11 12:43:25 -0600

    Actualizacion_2025_03_11

---
+++ src/app/shared/components/footer-menu/footer-menu.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer-menu',
+  standalone: false,
+  templateUrl: './footer-menu.component.html',
+  styleUrl: './footer-menu.component.css'
+})
+export class FooterMenuComponent {
+
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeaderMenuComponent } from './components/header-menu/header-menu.component';
+import { FooterMenuComponent } from './components/footer-menu/footer-menu.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HeaderMenuComponent,
+    FooterMenuComponent
+  ],
   imports: [
     CommonModule
   ]
